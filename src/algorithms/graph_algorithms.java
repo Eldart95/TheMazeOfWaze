9783292e57cbb761849b60317e536444920851a6
YosefTwito <yosef@LAPTOP-IAVTIVD5>
2019-12-17 12:21:26 +0200

    grammar fix

--- src/algorithms/graph_algorithms.java
+++ src/algorithms/graph_algorithms.java
@@ -1,5 +1,10 @@
 package algorithms;
 
+import java.util.List;
+
+import dataStructure.graph;
+import dataStructure.node_data;
+
 /**
  * This interface represents the "regular" Graph Theory algorithms including:
  * 0. clone();
@@ -12,38 +17,39 @@ package algorithms;
  * @author YosefTwito
  */
 
-import java.util.List;
-
-import dataStructure.graph;
-import dataStructure.node_data;
-
 public interface graph_algorithms {
+	
 	/**
 	 * Init this set of algorithms on the parameter - graph.
 	 * @param g
 	 */
 	public void init(graph g);
+	
 	/** 
 	 * Compute a deep copy of this graph.
 	 * @return
 	 */
 	public graph copy();
+	
 	/**
 	 * Init a graph from file
 	 * @param file_name
 	 */
 	public void init(String file_name);
+	
 	/** 
 	 * Saves the graph to a file.
 	 * @param file_name
 	 */
 	public void save(String file_name);
-/**
- * Returns true if and only if (iff) there is a valid path from EVERY node to each other node. 
- * NOTE: assume directional graph - a valid path (a-->b) does NOT imply a valid path (b-->a).
- * @return
- */
+	
+	/**
+	* Returns true if and only if (iff) there is a valid path from EVERY node to each other node. 
+	* NOTE: assume directional graph - a valid path (a-->b) does NOT imply a valid path (b-->a).
+	* @return
+	*/
 	public boolean isConnected();
+	
 	/**
 	 * returns the length of the shortest path between src to dest
 	 * @param src - start node
@@ -51,6 +57,7 @@ public interface graph_algorithms {
 	 * @return
 	 */
 	public double shortestPathDist(int src, int dest);
+	
 	/**
 	 * returns the shortest path between src to dest - as an ordered List of nodes:
 	 * src--> n1-->n2-->...dest
@@ -60,6 +67,7 @@ public interface graph_algorithms {
 	 * @return
 	 */
 	public List<node_data> shortestPath(int src, int dest);
+	
 	/**
 	 * computes a relatively short path which visits each node in the targets List.
 	 * Note: this is NOT the classical traveling salesman problem, 
