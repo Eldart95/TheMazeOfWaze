d5d24090d23827b83cd2db90ab6d2cc3d5b6d22e
YosefTwito <yosef@LAPTOP-IAVTIVD5>
2019-12-17 12:29:33 +0200

    adding new classes

---
+++ src/Testing/Gui_Main.java
@@ -0,0 +1,10 @@
+package Testing;
+
+public class Gui_Main {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
---
+++ src/Testing/Test_DGraph.java
@@ -0,0 +1,24 @@
+package Testing;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+class Test_DGraph {
+
+	@Test
+	void testConnect() {
+		fail("Not yet implemented"); // TODO
+	}
+
+	@Test
+	void testRemoveNode() {
+		fail("Not yet implemented"); // TODO
+	}
+
+	@Test
+	void testRemoveEdge() {
+		fail("Not yet implemented"); // TODO
+	}
+
+}
