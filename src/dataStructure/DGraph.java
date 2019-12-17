c43fd821bd5f785c18cd44914a00be88240f41b9
YosefTwito <yosef@LAPTOP-IAVTIVD5>
2019-12-17 16:14:26 +0200

    a

--- src/dataStructure/DGraph.java
+++ src/dataStructure/DGraph.java
@@ -7,18 +7,24 @@ import java.util.Iterator;
 
 public class DGraph implements graph{
 
-	HashMap<Integer, node_data> nodeMap = new HashMap<>();
+	HashMap<Integer, node_data> nodeMap = new HashMap<Integer, node_data>();
 	ArrayList<edge> edges = new ArrayList<edge>();
+	
+	public DGraph() {
+		this.nodeMap = new HashMap<Integer, node_data>();
+		this.edges = new ArrayList<edge>();
+	}
+	
 
 	@Override
 	public node_data getNode(int key) { 
-		if (nodeMap.get(key)==null) { return null; }
-		return nodeMap.get(key); 
+		if (this.nodeMap.get(key)==null) { return null; }
+		return this.nodeMap.get(key); 
 	}
 
 	@Override
 	public edge_data getEdge(int src, int dest) {
-		node p = (node)nodeMap.get(src);
+		node p = (node)this.nodeMap.get(src);
 		if (p.edgeTo.get(dest) != null) { return p.edgeTo.get(dest); }
 		return null;
 	}
@@ -26,16 +32,16 @@ public class DGraph implements graph{
 	@Override
 	public void addNode(node_data n) {
 		int key=n.getKey();
-		nodeMap.put(key, n);
+		this.nodeMap.put(key, n);
 	}
 
 	@Override
 	public void connect(int src, int dest, double w) {
-		node s = (node)nodeMap.get(src);
-		node d = (node)nodeMap.get(dest);
+		node s = (node)this.nodeMap.get(src);
+		node d = (node)this.nodeMap.get(dest);
 		edge e = new edge(src, dest, w);
 
-		edges.add(e);
+		this.edges.add(e);
 
 		s.edgeTo.put(dest, e);
 		d.edgeFrom.put(src, e);
@@ -44,11 +50,11 @@ public class DGraph implements graph{
 	}
 
 	@Override
-	public Collection<node_data> getV() { return (Collection<node_data>)nodeMap; }
+	public Collection<node_data> getV() { return (Collection<node_data>)this.nodeMap; }
 
 	@Override
 	public Collection<edge_data> getE(int node_id) {
-		node_data n = nodeMap.get(node_id);
+		node_data n = this.nodeMap.get(node_id);
 		return (Collection<edge_data>)((node)n).edgeTo;
 	}
 
@@ -58,16 +64,16 @@ public class DGraph implements graph{
 	 * @return a collection of all the edges going towards our node.
 	 */
 	public Collection<edge_data> getEFrom(int node_id) {
-		node_data n = nodeMap.get(node_id);
+		node_data n = this.nodeMap.get(node_id);
 		return (Collection<edge_data>)((node)n).edgeFrom;
 	}
 
 	@Override
 	public node_data removeNode(int key) {/////////////////////// gotta check!!! ///////
 
-		if (nodeMap.get(key)==null) { return null; }
+		if (this.nodeMap.get(key)==null) { return null; }
 
-		node n = (node)(nodeMap.get(key));
+		node n = (node)(this.nodeMap.get(key));
 
 		//remove all the edges coming out of node(key) and their's link to nodes.
 		Collection<edge_data> toDeleteTo = getE(key);
@@ -89,23 +95,23 @@ public class DGraph implements graph{
 			removeEdge(s,d);
 		}
 
-		nodeMap.remove(key);
+		this.nodeMap.remove(key);
 		return n;
 	}
 
 	@Override
 	public edge_data removeEdge(int src, int dest) {//////////////////// O(1) ????????????????
-		node s = (node)nodeMap.get(src);
-		node d = (node)nodeMap.get(dest);
+		node s = (node)this.nodeMap.get(src);
+		node d = (node)this.nodeMap.get(dest);
 		if (s.edgeTo.get(dest) == null) { return null; }
 		edge_data e = s.edgeTo.get(dest); 
 
 		s.edgeTo.remove(dest);
 		d.edgeFrom.remove(src);
 
-		for (int i=0; i<edges.size(); i++) {
-			if (edges.get(i)==e) { 
-				edges.remove(i);
+		for (int i=0; i<this.edges.size(); i++) {
+			if (this.edges.get(i)==e) { 
+				this.edges.remove(i);
 				return e;
 			}
 		}
@@ -113,11 +119,11 @@ public class DGraph implements graph{
 	}
 
 	@Override
-	public int nodeSize() { return nodeMap.size(); }
+	public int nodeSize() { return this.nodeMap.size(); }
 
 
 	@Override
-	public int edgeSize() { return edges.size(); }
+	public int edgeSize() { return this.edges.size(); }
 
 	@Override
 	public int getMC() {
