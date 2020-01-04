2b629b75ee421024ad0fcaf05524c38eb9f00610
YosefTwito <yosef@LAPTOP-IAVTIVD5>
2020-01-04 23:29:34 +0200

    Done.

--- src/gui/Gui_Main.java
+++ src/gui/Gui_Main.java
@@ -1,19 +1,13 @@
 package gui;
 
-import dataStructure.DGraph;
-import dataStructure.graph;
-import gui.graphFactory;
+import dataStructure.*;
 
 public class Gui_Main {
 
 	public static void main(String[] args) {
 
 		graphFactory r = new graphFactory();
-		graph g= new DGraph(); // = r.randomGraphSmallConnected();
-		
-		g.addNode(r.nodeGenerator());
-		g.addNode(r.nodeGenerator());
-		g.connect(0, 1, 2);
+		graph g = r.randomGraphSmallConnected();
 		
 		GraphGui a = new GraphGui(g);
 		a.setVisible(true);
