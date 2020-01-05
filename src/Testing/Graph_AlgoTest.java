96c3b811ae8b9eb91fe458af2e288ff9c498e0a2
YosefTwito <yosef@LAPTOP-IAVTIVD5>
2020-01-05 19:12:50 +0200

    Eldi

---
+++ src/Testing/Graph_AlgoTest.java
@@ -0,0 +1,34 @@
+package Testing;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+class Graph_AlgoTest {
+
+	@Test
+	void testInitSave() {
+		fail("Not yet implemented"); // TODO
+	}
+
+	@Test
+	void testIsConnected() {
+		fail("Not yet implemented"); // TODO
+	}
+
+	@Test
+	void testShortestPathDist() {
+		fail("Not yet implemented"); // TODO
+	}
+
+	@Test
+	void testShortestPath() {
+		fail("Not yet implemented"); // TODO
+	}
+
+	@Test
+	void testTSP() {
+		fail("Not yet implemented"); // TODO
+	}
+
+}
