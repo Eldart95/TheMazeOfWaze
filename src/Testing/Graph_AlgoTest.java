0151181d17f8899e1f88c06aec4de53fb7578bc6
eldar <eldartakach@gmail.com>
2020-01-05 20:40:47 +0200

    testing added

--- src/Testing/Graph_AlgoTest.java
+++ src/Testing/Graph_AlgoTest.java
@@ -3,9 +3,13 @@ package Testing;
 import static org.junit.Assert.fail;
 import static org.junit.jupiter.api.Assertions.*;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import org.junit.jupiter.api.Test;
 
 import algorithms.Graph_Algo;
+import dataStructure.DGraph;
 import dataStructure.graph;
 import dataStructure.node;
 import dataStructure.node_data;
@@ -13,7 +17,7 @@ import gui.graphFactory;
 
 class Graph_AlgoTest {
 
-	@Test
+//	@Test
 	void testInitSave() {
 	graphFactory g = new graphFactory();
 	graph t = g.randomGraphLarge();
@@ -25,21 +29,55 @@ class Graph_AlgoTest {
 	
 	}
 
-	@Test
+//	@Test
 	void testIsConnected() {
 		graphFactory gf = new graphFactory();
-		Graph_Algo ga = new Graph_Algo(gf.randomGraphSmallConnected());
+		graph t = gf.randomGraphSmallConnected();
+		Graph_Algo ga = new Graph_Algo(t);
 		if (!(ga.isConnected())) { fail(); }
 	}
 
-	@Test
+//	@Test
 	void testShortestPathDist() {
-		fail("Not yet implemented"); // TODO
+		graphFactory x = new graphFactory();
+		node_data a1 = x.nodeGenerator();
+		node_data a2 = x.nodeGenerator();
+		node_data a3 = x.nodeGenerator();
+		node_data a4 = x.nodeGenerator();
+		
+		DGraph ng = new DGraph();
+		ng.addNode(a1);
+		ng.addNode(a2);
+		ng.addNode(a3);
+		ng.addNode(a4);
+		
+		ng.connect(a1.getKey(), a2.getKey(), 3);
+		ng.connect(a2.getKey(), a3.getKey(), 5);
+		ng.connect(a4.getKey(), a1.getKey(), 8);
+		ng.connect(a3.getKey(), a4.getKey(), 1);
+		Graph_Algo n=new Graph_Algo(ng);
+		assertEquals(n.shortestPathDist(a1.getKey(), a4.getKey()),9);
+		
 	}
 
 	@Test
 	void testShortestPath() {
-		fail("Not yet implemented"); // TODO
+		graphFactory gf = new graphFactory();
+		graph t = gf.randomGraphSmallConnected();
+		Graph_Algo ga = new Graph_Algo(t);
+		List<node_data> l = ga.shortestPath(2, 5);
+		List<Integer> k = new ArrayList<Integer>();
+		k.add(2);
+		k.add(4);
+		k.add(8);
+		k.add(9);
+		k.add(6);
+		k.add(5);
+		if(l.size()!=k.size()) {fail();}
+		for(int i=0;i<l.size();i++) {
+			if(l.get(i).getKey()!=k.get(i)) {fail();}
+				
+		}
 	}
 
 	@Test
